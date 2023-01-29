9809cd43540094d3fcffb330907b052dc46acb67
Zimin Alexander <grobovozik@mail.ru>
2023-01-29 18:19:09 +0300

    скорректировал преобразование даты

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,18 +1,13 @@
-export function dateFormat(date){
-    const dateVocabulary = {
-      Jun: "января",
-      Feb: "февраля",
-      Mar: "марта",
-      Apr: "апреля",
-      May: "мая",
-      Jun: "июня",
-      Jul: "июля",
-      Aug: "августа",
-      Sep: "сентября",
-      Oct: "октября",
-      Nov: "ноября",
-      Dec: "декабря",
-    };
-    const splitDate = date.split(' ');
-    return splitDate[2] + " " + dateVocabulary[splitDate[1]] + " " + splitDate[3];
+export function dateFormat(dateString){
+    const dateObject = new Date(dateString);
+    const numberOfMonth = dateObject.getMonth();
+    const dayOfMonth = dateObject.getDay();
+    const year = dateObject.getFullYear();
+    const monthVocabulary = ["января", "февраля",
+      "марта", "апреля", "мая", "июня",
+      "июля", "августа", "сентября",
+      "октября", "ноября", "декабря",
+    ];
+
+    return dayOfMonth + " " + monthVocabulary[numberOfMonth] + " " + year + " г.";
   };
