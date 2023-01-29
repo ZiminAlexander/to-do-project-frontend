2a83f69b12b690d50f634746fba53928b65bc785
Zimin Alexander <grobovozik@mail.ru>
2023-01-29 18:36:49 +0300

    вынес массив наименований месяца

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,13 +1,9 @@
+import {monthVocabulary} from "./monthVocabulary";
+
 export function dateFormat(dateString){
     const dateObject = new Date(dateString);
     const numberOfMonth = dateObject.getMonth();
     const dayOfMonth = dateObject.getDay();
     const year = dateObject.getFullYear();
-    const monthVocabulary = ["января", "февраля",
-      "марта", "апреля", "мая", "июня",
-      "июля", "августа", "сентября",
-      "октября", "ноября", "декабря",
-    ];
-
     return dayOfMonth + " " + monthVocabulary[numberOfMonth] + " " + year + " г.";
   };
