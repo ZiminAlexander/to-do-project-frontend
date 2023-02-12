c0554b80ed60d434ea3c5420636e653687cb078f
ZiminAlex <36967526+ZiminAlexander@users.noreply.github.com>
2023-02-12 20:11:25 +0300

    Исправлено отображение дня месяца (#26)

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -3,7 +3,7 @@ import {monthVocabulary} from "./monthVocabulary";
 export function dateFormat(dateString){
     const dateObject = new Date(dateString);
     const numberOfMonth = dateObject.getMonth();
-    const dayOfMonth = dateObject.getDay();
+    const dayOfMonth = dateObject.getDate();
     const year = dateObject.getFullYear();
     return dayOfMonth + " " + monthVocabulary[numberOfMonth] + " " + year + " г.";
   };
