00cdc193e9891efcdac1622c60c2759c13430f1b
Zimin Alexander <grobovozik@mail.ru>
2023-02-12 11:47:11 +0300

    Скорректирована логика вызова окна редактирования

--- src/components/edit-window/edit-window.js
+++ src/components/edit-window/edit-window.js
@@ -3,7 +3,7 @@ import { createEditTable } from "./edit-table/edit-table";
 import "./edit-window.css";
 
 export function addEditWindow(){
-    const editWindow = createNewElement("div",["edit-window", "hidden"]);
+    const editWindow = createNewElement("div","edit-window");
     editWindow.append(createEditTable());
 
     document.body.append(editWindow);
