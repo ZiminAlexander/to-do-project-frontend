471dac57b4784b9e8ecc77a0675d59ebdbc11ef3
Zimin Alexander <grobovozik@mail.ru>
2023-02-16 23:04:46 +0300

    попытка решить конфликт

--- src/components/task-table/task/delete-task-button/delete-task-button.js
+++ src/components/task-table/task/delete-task-button/delete-task-button.js
@@ -2,6 +2,7 @@ import { createNewElement } from "Project/helpers/createNewElement";
 import { updateTasksFromServer } from "../task";
 import { talkWithServer } from "Project/api/talkWithServer";
 import { addSpinner } from "Project/helpers/addSpinner";
+import { showNotification } from "Project/components/notifications/notifications";
 import "./delete-task-button.css";
 
 export function createDeleteTaskButton() {
@@ -11,13 +12,23 @@ export function createDeleteTaskButton() {
     "delete-task-button",
   ]);
   newDeleteElement.addEventListener("click", function () {
-    addSpinner("on", this);
-    this.disabled = "true";
-    talkWithServer("DELETE", this.parentElement).then(() => {
-      addSpinner("off", this);
-      updateTasksFromServer();
-    });
+    const currentDeleteButton = this;
+    showNotification(
+      "Задача будет удалена без возможности восстановления. Вы точно хотите удалить задачу?",
+      "confirm",
+      () => {
+        deleteTask(currentDeleteButton);
+      }
+    );
   });
-
   return newDeleteElement;
+}
+
+function deleteTask(deleteButton) {
+  addSpinner("on", deleteButton);
+  deleteButton.disabled = "true";
+  talkWithServer("DELETE", deleteButton.parentElement).then(() => {
+    addSpinner("off", deleteButton);
+    updateTasksFromServer();
+  });
 }
