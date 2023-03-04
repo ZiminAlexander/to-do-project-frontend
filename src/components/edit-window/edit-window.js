f0b8e45ac07972b783c33aef78d7a756cbdf2278
Zimin Alexander <grobovozik@mail.ru>
2023-03-04 15:53:03 +0300

    теперь нельзя открыть 2 окна редактирования

--- src/components/edit-window/edit-window.js
+++ src/components/edit-window/edit-window.js
@@ -1,8 +1,13 @@
 import { createNewElement } from "Project/helpers/createNewElement";
 import { createEditTable } from "./edit-table/edit-table";
+import { closeEditWindow } from "./edit-table/edit-table-box/close-edit-button/close-edit-button";
 import "./edit-window.css";
 
 export function addEditWindow() {
+  const existedEditWindow = document.querySelector(".edit-window");
+  if (existedEditWindow) {
+    closeEditWindow();
+  }
   const editWindow = createNewElement("div", "edit-window");
   editWindow.append(createEditTable());
 
